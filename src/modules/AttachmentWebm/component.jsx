6fd1f2ec5dff5bdabcea125fdd938920b0507899
aqemi <2773683+aqemi@users.noreply.github.com>
2026-02-23 00:52:30 +0200

    feat: enhance AttachmentWebm component with thumbnail loading and video state management

--- src/modules/AttachmentWebm/component.jsx
+++ src/modules/AttachmentWebm/component.jsx
@@ -3,64 +3,90 @@
 import React, { Component } from 'react';
 import IconButton from '@mui/material/IconButton';
 import VideoFile from '@mui/icons-material/VideoFile';
+import BrokenImage from '@mui/icons-material/BrokenImage';
 import { getExtWebmThumbnail } from '../../utils';
 
 export default class AttachmentWebm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      expanded: false
+      expanded: false,
+      thumbWidth: null,
+      thumbHeight: null,
+      videoLoaded: false
     };
+    this.handleThumbLoad = this.handleThumbLoad.bind(this);
+    this.handleVideoLoaded = this.handleVideoLoaded.bind(this);
   }
 
   toggleExpand() {
+    this.setState(state => ({
+      expanded: !state.expanded,
+      videoLoaded: false
+    }));
+  }
+
+  handleThumbLoad(e) {
     this.setState({
-      expanded: !this.state.expanded
+      thumbWidth: e.target.naturalWidth,
+      thumbHeight: e.target.naturalHeight
     });
   }
 
+  handleVideoLoaded() {
+    this.setState({ videoLoaded: true });
+  }
 
   render() {
     const { extWebmUrl } = this.props;
     const thumbnailUrl = getExtWebmThumbnail(extWebmUrl);
 
     const icon = (
-      <IconButton
-        className='attachment__icon'
-        variant='overlay'
-        onClick={this.toggleExpand.bind(this)}
-      >
+      <IconButton className='attachment__icon' variant='overlay' onClick={this.toggleExpand.bind(this)}>
         <VideoFile />
       </IconButton>
     );
 
     if (this.state.expanded) {
+      const { thumbWidth, thumbHeight, videoLoaded } = this.state;
+      const videoStyle = !videoLoaded && thumbWidth ? { width: thumbWidth, height: thumbHeight } : {};
       return (
-        <div className='attachment attachment--webm'>
-          <video
-            controls
-            autoPlay
-          >
-            <source src={extWebmUrl} type='video/webm' />
-          </video>
-          <a
-            href=''
-            onClick={(e) => { e.preventDefault(); this.toggleExpand(); }}
-          >
-            Закрыть
-          </a>
+        <div>
+          <div className='attachment attachment--webm'>
+            <div className='attachment__inline'>
+              <video controls autoPlay style={videoStyle} onLoadedData={this.handleVideoLoaded}>
+                <source srwsc={extWebmUrl} type='video/webm' />
+              </video>
+              <a
+                href=''
+                onClick={(e) => {
+                  e.preventDefault();
+                  this.toggleExpand();
+                }}
+              >
+                Закрыть
+              </a>
+            </div>
+          </div>
         </div>
       );
     }
 
+    const thumbLoaded = this.state.thumbWidth !== null;
+
     return (
       <div className='attachment attachment--webm'>
-        <img
-          alt='webm'
-          src={thumbnailUrl}
-          onClick={this.toggleExpand.bind(this)}
-        />
-        {icon}
+        <div className='attachment__inline'>
+          <img
+            alt='webm'
+            src={thumbnailUrl}
+            onLoad={this.handleThumbLoad}
+            onClick={this.toggleExpand.bind(this)}
+            style={thumbLoaded ? {} : { display: 'none' }}
+          />
+          {!thumbLoaded && <div className='attachment__thumb-placeholder'><BrokenImage /></div>}
+          {thumbLoaded && icon}
+        </div>
       </div>
     );
   }
