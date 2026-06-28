89d629344e5680f85130077a2eed2c96e0d572c6
aqemi <2773683+aqemi@users.noreply.github.com>
2026-06-28 21:18:35 +0300

    fix quote markup

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const MARKUP_STRONG_REGEXP = /\*\*(.+?)\*\*/g;
 export const MARKUP_EMPH_REGEXP = /\*(.+?)\*/g;
 export const MARKUP_STRIKE_REGEXP = /\[s\](.+?)\[\/s\]/g;
 export const MARKUP_SPOILER_REGEXP = /%%(.+?)%%/g;
-export const MARKUP_QUOTE_REGEXP = /(>.+|&gt;.+)/gm;
+export const MARKUP_QUOTE_REGEXP = /(>(?:(?!\/\/|\n).)+|&gt;(?:(?!\/\/|\n).)+)/gm;
 export const MARKUP_ADM_REGEXP = /(\$ADM\$)/g;
 export const MARKUP_MOD_REGEXP = /(\$MOD\$)/g;
 export const YOUTUBE_REGEXP = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})(?:(?:.+?)t=(\d+))?/; //eslint-disable-line
