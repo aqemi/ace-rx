88467c7e063ede74f90efbac7fd72725011588b8
aqemi <2773683+aqemi@users.noreply.github.com>
2026-07-08 17:01:07 +0300

    Revert "fix(logs): improve error handling and timezone adjustment in loadLog function"
    This reverts commit b978709cd377486804c08f0d126bb895e056f54b.

--- src/modules/Chat/api.js
+++ src/modules/Chat/api.js
@@ -57,8 +57,8 @@ export async function control(method, messageId, params = {}) {
 export function loadLog(date) {
   const d = new Date(date);
   const formattedDate = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
-  const tz = -new Date().getTimezoneOffset();
-  return fetch(`${LOG_ENDPOINT}&log=${formattedDate}&utc_offset_minutes=${tz}`, {
+  const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  return fetch(`${LOG_ENDPOINT}&log=${formattedDate}&timezone=${encodeURIComponent(tz)}`, {
     credentials: 'include'
   }).then((response) => response.text());
 }
